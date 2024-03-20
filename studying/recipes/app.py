f6a530c20eb2059a3d229f09b6d1536a7b3ceb83
Dennis Gera <geradennis@gmail.com>
2024-03-20 06:28:59 -0300

    feat: recipe

--- studying/recipes/app.py
+++ studying/recipes/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
+from flask import Flask, render_template
 from helper import (
     recipes,
+    types,
     descriptions,
     ingredients,
     instructions,
@@ -11,36 +9,40 @@ from helper import (
     add_instructions,
     comments,
 )
+from forms import RecipeForm, CommentForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
 
-class CommentForm(FlaskForm):
-    comment = StringField("Comment")
-    submit = SubmitField("Submit")
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    new_id = len(recipes) + 1
-    if len(request.form) > 0:
-        recipes[new_id] = request.form["recipe"]
-        descriptions[new_id] = request.form["description"]
-        new_ingredients = request.form["ingredients"]
-        new_instructions = request.form["instructions"]
+    recipe_form = RecipeForm(csrf_enabled=False)
+    if recipe_form.validate_on_submit():
+        new_id = len(recipes) + 1
+        recipes[new_id] = recipe_form.recipe.data
+        types[new_id] = recipe_form.recipe_type.data
+        descriptions[new_id] = recipe_form.description.data
+        new_ingredients = recipe_form.ingredients.data
+        new_instructions = recipe_form.instructions.data
         add_ingredients(new_id, new_ingredients)
         add_instructions(new_id, new_instructions)
-    return render_template("index.html", template_recipes=recipes)
+        comments[new_id] = []
+    return render_template(
+        "index.html", template_recipes=recipes, template_form=recipe_form
+    )
 
 
 @app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-    #### Instantiate form class here
-    comment_form = CommentForm()
+    comment_form = CommentForm(csrf_enabled=False)
+    if comment_form.validate_on_submit():
+        new_comment = comment_form.comment.data
+        comments[id].append(new_comment)
     return render_template(
         "recipe.html",
         template_recipe=recipes[id],
+        template_type=types[id],
         template_description=descriptions[id],
         template_ingredients=ingredients[id],
         template_instructions=instructions[id],
